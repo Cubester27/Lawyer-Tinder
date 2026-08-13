88a020435b9c43fff9497dc44e8034c3cd4c81f4
cubester27 <sarankan17@gmail.com>
2026-08-13 10:02:24 +0200

    Revert "bugfix import server dynamically"
    This reverts commit 1eefc04208ddb33abd4baae29d5a83ae85d64d45.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
+import app from '../backend/server.js';
+
 export default async function handler(req, res) {
   try {
-    const { default: app } = await import('../backend/server.js');
     return await app(req, res);
   } catch (err) {
     console.error('Vercel Serverless Function error:', err);
